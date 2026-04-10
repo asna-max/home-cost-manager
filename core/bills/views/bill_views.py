76e526e663b840399d65731435dcb1f6b442c81f
asna-max <81409289+asna-max@users.noreply.github.com>
2026-04-10 12:16:21 +0200

    Restrict Bill API to authenticated user's households

--- core/bills/views/bill_views.py
+++ core/bills/views/bill_views.py
@@ -4,6 +4,7 @@ from bills.models import Bill
 from bills.serializers.bill_serializer import BillSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework import status
+from households.models import HouseholdMember
 
 
 class BillListView(APIView):
@@ -13,12 +14,21 @@ class BillListView(APIView):
         if not household_id:
             return Response({"error": "household parameter is required"}, status=400)
 
-        bills = Bill.objects.filter(household_id=household_id)
+        bills = Bill.objects.filter(
+            household_id=household_id, household__householdmember__user=request.user)
+
+        if not bills.exists():
+            return Response({"error": "No access to this household"}, status=403)
 
         serializer = BillSerializer(bills, many=True)
         return Response(serializer.data)
 
     def post(self, request):
+        household_id = request.query_params.get('household')
+
+        if not HouseholdMember.objects.filter(household_id=household_id, user=request.user).exists():
+            return Response({"error": "not allower"}, status=403)
+
         serializer = BillSerializer(data=request.data)
 
         if serializer.is_valid():
--- core/households/models.py
+++ core/households/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 
+
 class Household(models.Model):
     name = models.CharField(max_length=255)
 
@@ -9,7 +10,8 @@ class Household(models.Model):
 
 
 class HouseholdMember(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     household = models.ForeignKey(Household, on_delete=models.CASCADE)
 
     ROLE_CHOICES = (
@@ -17,11 +19,12 @@ class HouseholdMember(models.Model):
         ('member', 'Member'),
     )
 
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='member')
+    role = models.CharField(
+        max_length=10, choices=ROLE_CHOICES, default='member')
     joined_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         unique_together = ('user', 'household')
-    
+
     def __str__(self):
         return f"{self.user} - {self.household} ({self.role})"
