b5bd10086aa88d578f7a920fa31f8d1b36158cfa
asna-max <81409289+asna-max@users.noreply.github.com>
2026-04-22 12:38:35 +0200

    add filter logic for bills

--- frontend/src/pages/Bills.jsx
+++ frontend/src/pages/Bills.jsx
@@ -4,6 +4,9 @@ import { BASE_URL } from "../services/api";
 
 export default function Bills({ token, selectedHousehold }) {
   const [bills, setBills] = useState([]);
+  const [typeFilter, setTypeFilter] = useState("all");
+  const [statusFilter, setStatusFilter] = useState("all");
+  const [yearFilter, setYearFilter] = useState("all");
 
   // Daten laden
   useEffect(() => {
@@ -40,11 +43,78 @@ export default function Bills({ token, selectedHousehold }) {
     );
   };
 
+  const filteredBills = bills.filter((bill) => {
+    // Type
+    if (typeFilter !== "all" && bill.bill_type != typeFilter) {
+      return false;
+    }
+
+    // Status
+    if (statusFilter === "paid" && !bill.is_paid) return false;
+    if (statusFilter === "unpaid" && bill.is_paid) return false;
+
+    // Year
+    if (yearFilter !== "all") {
+      const billyear = new Date(bill.period_from).getFullYear().toString();
+      if (billyear !== yearFilter) return false;
+    }
+
+    return true;
+  });
+
+  const year = [
+    ...new Set(bills.map((b) => new Date(b.period_from).getFullYear())),
+  ].sort((a, b) => b - a);
+
   return (
     <div>
       <h2>Bills</h2>
+      <div style={{ marginBottom: "15px" }}>
+        {/* TYPE */}
+        <select
+          value={typeFilter}
+          onChange={(e) => setTypeFilter(e.target.value)}
+        >
+          <option value="all">All Types</option>
+          <option value="electricity">Electricity</option>
+          <option value="water">Water</option>
+          <option value="heating">Heating</option>
+          <option value="other">Other</option>
+        </select>
+        {/* STATUS */}
+        <select
+          // value={statusFilter}
+          onChange={(e) => setStatusFilter(e.target.value)}
+        >
+          <option value="all">All Status</option>
+          <option value="paid">Paid</option>
+          <option value="unpaid">Unpaid</option>
+        </select>
 
-      {bills.length === 0 ? (
+        {/* YEAR */}
+        <select
+          value={yearFilter}
+          onChange={(e) => setYearFilter(e.target.value)}
+        >
+          <option value="all">All Years</option>
+
+          {year.map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button
+        onClick={() => {
+          setTypeFilter("all");
+          setStatusFilter("all");
+          setYearFilter("all");
+        }}
+      >
+        Reset Filters
+      </button>
+      {filteredBills.length === 0 ? (
         <p>No bills found</p>
       ) : (
         <table border="1" cellPadding="10">
@@ -64,7 +134,7 @@ export default function Bills({ token, selectedHousehold }) {
           </thead>
 
           <tbody>
-            {bills.map((bill) => (
+            {filteredBills.map((bill) => (
               <tr key={bill.id}>
                 <td>{bill.title}</td>
                 <td>{bill.bill_type}</td>
