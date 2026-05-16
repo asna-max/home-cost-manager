303c0fadeafa9f8f4307bbedfb56fb3e877c17c7
A.Nadesu <a.nadesu@monnier-zahner.ch>
2026-05-16 11:30:30 +0200

    feat(bills): add dark mode support to bills page

--- frontend/src/features/bills/Bills.jsx
+++ frontend/src/features/bills/Bills.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+
 import { useHousehold } from "../../shared/hooks/useHousehold";
+
 import { useBills } from "./hooks/useBills";
+
 import { getYears, filterBills } from "./utils/billsUtils";
 
 import BillsFilters from "./components/BillFilters";
@@ -8,8 +11,13 @@ import BillsTable from "./components/BillsTable";
 
 export default function Bills() {
   const { selectedHousehold } = useHousehold();
+
   const { bills, loading, removeBill, toggleStatus } =
     useBills(selectedHousehold);
+
+  // =========================
+  // FILTERS
+  // =========================
 
   const [filters, setFilters] = useState({
     type: "all",
@@ -18,13 +26,52 @@ export default function Bills() {
   });
 
   const years = getYears(bills);
+
   const filtered = filterBills(bills, filters);
 
+  // =========================
+  // NO HOUSEHOLD
+  // =========================
+
+  if (!selectedHousehold) {
+    return (
+      <div
+        className="
+          text-center
+          mt-10
+          text-gray-500
+          dark:text-gray-400
+        "
+      >
+        Select household
+      </div>
+    );
+  }
+
+  // =========================
+  // PAGE
+  // =========================
+
   return (
-    <div>
-      {loading && <p>Loading...</p>}
+    <div className="space-y-6">
+      {/* LOADING */}
+
+      {loading && (
+        <p
+          className="
+            text-gray-500
+            dark:text-gray-400
+          "
+        >
+          Loading...
+        </p>
+      )}
+
+      {/* FILTERS */}
 
       <BillsFilters filters={filters} setFilters={setFilters} years={years} />
+
+      {/* TABLE */}
 
       <BillsTable
         bills={filtered}
