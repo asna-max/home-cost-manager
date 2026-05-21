ddaf416528e944adecf5f45f0d7b39abe30b2e74
A.Nadesu <a.nadesu@monnier-zahner.ch>
2026-05-21 10:29:09 +0200

    test(permissions): add non-member household access test

--- core/households/tests/test_permissions.py
+++ core/households/tests/test_permissions.py
@@ -5,6 +5,11 @@ from rest_framework.test import (
 )
 from users.models import User
 
+from households.models import (
+    Household,
+    HouseholdMember,
+)
+
 
 class HouseholdPermissionTests(
     APITestCase,
@@ -45,4 +50,49 @@ class HouseholdPermissionTests(
         self.assertNotEqual(
             response.status_code,
             status.HTTP_401_UNAUTHORIZED,
+        )
+
+    def test_non_member_cannot_access_household(
+        self,
+    ):
+        # USERS
+        owner = User.objects.create_user(
+            username="owner",
+            email="owner@test.com",
+            password="Test1234!",
+        )
+
+        stranger = User.objects.create_user(
+            username="stranger",
+            email="stranger@test.com",
+            password="Test1234!",
+        )
+
+        # HOUSEHOLD
+        household = Household.objects.create(
+            name="Test Household",
+            owner=owner,
+        )
+
+        # MEMBER
+        HouseholdMember.objects.create(
+            user=owner,
+            household=household,
+            role="owner",
+        )
+
+        # LOGIN AS STRANGER
+        self.client.force_authenticate(
+            user=stranger,
+        )
+
+        # REQUEST
+        response = self.client.get(
+            f"/api/households/{household.id}/",
+        )
+
+        # EXPECT FORBIDDEN
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_403_FORBIDDEN,
         )
