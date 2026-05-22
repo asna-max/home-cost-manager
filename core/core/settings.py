58ee65fbbb7bd9dcda89b48ead7f27714d783b38
A.Nadesu <a.nadesu@monnier-zahner.ch>
2026-05-22 14:45:41 +0200

    chore(config): prepare project for PostgreSQL support

--- core/core/settings.py
+++ core/core/settings.py
@@ -110,13 +110,31 @@ TEMPLATES = [
 # =========================
 # DATABASE
 # =========================
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+USE_POSTGRES = os.getenv(
+    "USE_POSTGRES",
+    "False",
+) == "True"
+
+if USE_POSTGRES:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT"),
+        }
+    }
+
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # =========================
 # PASSWORD VALIDATION
