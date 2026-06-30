aab540f91f310d55c07369fad39528e8b0475d1b
A.Nadesu <a.nadesu@monnier-zahner.ch>
2026-06-30 16:07:03 +0200

    chore(frontend): add environment based api configuration

--- frontend/src/services/api/axios.js
+++ frontend/src/services/api/axios.js
@@ -14,7 +14,7 @@ import {
 
 //export const API_BASE = "http://10.77.164.57:8000";
 //export const API_BASE = "http://127.0.0.1:8000";
-const API_BASE = import.meta.env.VITE_API_URL;
+export const API_BASE = import.meta.env.VITE_API_URL;
 
 // =========================
 // AXIOS INSTANCE
